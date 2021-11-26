034632e4f100c2bec97a3b3fa161cdb671a8249e
Pedro Barbeira <up201303693@fe.up.pt>
2021-11-26 00:01:21 +0000

    Took ./cmake-build-debug out of .gitignore so the login functions work after pulling from repository

---
+++ cmake-build-debug/CMakeFiles/mikegairlines.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Coding/Cpp/AEDA/mikegairlines/date.cpp" "C:/Coding/Cpp/AEDA/mikegairlines/cmake-build-debug/CMakeFiles/mikegairlines.dir/date.cpp.obj"
+  "C:/Coding/Cpp/AEDA/mikegairlines/frontend/mainmenu.cpp" "C:/Coding/Cpp/AEDA/mikegairlines/cmake-build-debug/CMakeFiles/mikegairlines.dir/frontend/mainmenu.cpp.obj"
+  "C:/Coding/Cpp/AEDA/mikegairlines/main.cpp" "C:/Coding/Cpp/AEDA/mikegairlines/cmake-build-debug/CMakeFiles/mikegairlines.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././airport"
+  ".././frontend"
+  ".././passenger"
+  ".././voyage"
+  ".././credentials"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/mikegairlines.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mikegairlines.dir/date.cpp.obj"
+  "CMakeFiles/mikegairlines.dir/frontend/mainmenu.cpp.obj"
+  "CMakeFiles/mikegairlines.dir/main.cpp.obj"
+  "libmikegairlines.dll.a"
+  "mikegairlines.exe"
+  "mikegairlines.exe.manifest"
+  "mikegairlines.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mikegairlines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
