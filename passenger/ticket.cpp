d116ffa6bbf5c95c07cb39c74f2eff699a7d30d9
up202003484 <up202003484@fe.up.pt>
2021-12-03 14:53:57 +0000

    ticket

--- passenger/ticket.cpp
+++ passenger/ticket.cpp
@@ -2,10 +2,36 @@
 
 Ticket::Ticket(int number){
     this->number=number;
-
 }
 
 int Ticket::getNumber(){
     return number;
 }
+
+Voyage* Ticket::getOrigin() const {
+    return origin;
+}
+
+Voyage* Ticket::getDestination() const{
+    return destination;
+}
+
+Passenger* Ticket::getOwner() const {
+    return owner;
+}
+
+void Ticket::setOrigin(Voyage* voyage) {
+    origin=voyage;
+}
+
+void Ticket::setDestination(Voyage *voyage2) {
+    destination=voyage2;
+}
+
+void Ticket::setOwner(Passenger *voyage3) {
+    owner=voyage3;
+}
+
+
+
 
--- passenger/ticket.h
+++ passenger/ticket.h
@@ -2,24 +2,27 @@
 #ifndef MIKEGAIRLINES_TICKET_H
 #define MIKEGAIRLINES_TICKET_H
 #include <iostream>
+#include "voyage.h"
+#include "passenger.h"
 
 class Ticket {
     int number;
-    class Voyage* origin;
-    class Voyage* destination;
-    class Passenger* owner;
+    Voyage* origin;
+    Voyage* destination;
+    Passenger* owner;
 
 
 public:
     Ticket(int number);
     int getNumber();
+    Voyage* getOrigin() const;
+    Voyage* getDestination() const;
+    Passenger* getOwner() const;
+    void setOrigin(Voyage* voyage);
+    void setDestination(Voyage* voyage2);
+    void setOwner(Passenger* voyage3);
 
 };
+
+
 #endif //MIKEGAIRLINES_TICKET_H
-
-//lugar: *Voo
-
-//origem: *Voo
-//destino: *Voo
-
-//lugar: Lugar*
