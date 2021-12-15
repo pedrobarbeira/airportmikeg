81c7e074e8f9a8dfc6bdf086ad85e5dce2ebbb17
Pedro Barbeira <up201303693@fe.up.pt>
2021-12-15 18:26:33 +0000

    Touched up final stages of LogSort

--- src/sorter.h
+++ src/sorter.h
@@ -9,6 +9,7 @@ template<class T>
 class Sorter{
 protected:
     T container;
+    bool func(T a, T b);
 public:
     explicit Sorter(){};
     explicit Sorter(T data){
@@ -21,6 +22,9 @@ public:
     LogSorter(){
         container.clear();
     };
+    static bool recent(std::string s1, std::string s2){
+        return s1 < s2;
+    }
     std::vector<string> sort();
     std::vector<std::string> operator()(std::vector<std::string> data){
         container = data;
