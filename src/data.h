43b257cecd77a4a2c14f4e65837a28870d56510b
Pedro Barbeira <up201303693@fe.up.pt>
2021-12-17 01:39:03 +0000

    Commented Data methods

--- src/data.h
+++ src/data.h
@@ -23,57 +23,171 @@ class Data{
     friend class LoadUser;
     friend class SaveUser;
 public:
+    /**
+     * Constructor for Data class. Initializes each BST
+     */
     Data() : airports(AirportPointer(nullptr)), planes(PlanePointer(nullptr)),
              clients(ClientPointer(nullptr)), company(CompanyPointer(nullptr)),
              voyages(VoyagePointer(nullptr)), tickets(TicketPointer(nullptr)){}
 
+    /**
+     * Converts the airports BST into a vector of AirportPointer objects
+     * @return a vector with the data stored in the airports BST
+     */
     std::vector<AirportPointer> getAirports() const;
+
+    /**
+     * Converts the voyages BST into a vector of Voyage* objects
+     * @return a vector with the data stored in the voyages BST
+     */
     std::vector<Voyage*> getVoyages() const;
+
+    /**
+     * Iterates the airports BST and returns a vector of all the FlightPointer
+     * objects containing all flights
+     * @return a vector with all the FlightPointer objects in the system
+     */
     std::vector<FlightPointer> getFlights() const;
+
+    /**
+     * Converts the planes BST into a vector of Plane* objects
+     * @return a vector with the data stored in the planes BST
+     */
     std::vector<Plane*> getPlanes() const;
+
+    /**
+     * Converts the tickets BST into a vector of Ticket* objects
+     * @return a vector with the data stored in the tickets BST
+     */
     std::vector<Ticket*> getTickets() const;
+
+    /**
+     * Converts the clients BST into a vector of Client* objects
+     * @return a vector with the data stored in the clients BST
+     */
     std::vector<Client*> getClients() const;
+
+    /**
+     * Converts the company BST into a vector of Company* objects
+     * @return a vector with the data stored in the company BST
+     */
     std::vector<Company*> getCompany() const;
 
+    /**
+     * Returns the airports BST
+     * @return the airports BST
+     */
     BST<AirportPointer> getAirportBST() const{
         return airports;
     }
+
+    /**
+     * Returns the voyages BST
+     * @return the voyages BST
+     */
     BST<VoyagePointer> getVoyageBST() const{
         return voyages;};
+
+    /**
+     * Returns the planes BST
+     * @return the planes BST
+     */
     BST<PlanePointer> getPlaneBST() const{
         return planes;};
+
+    /**
+     * Creates a FlightPointer BST and iterates through the airports BST
+     * adding all FlightPointer objects of the system into the FlightPointer BST
+     * @return the created FlightPointer BSt
+     */
     BST<FlightPointer> getFlightBST() const;
+
+    /**
+     * Returns the tickets BST
+     * @return the tickets BST
+     */
     BST<TicketPointer> getTicketsBST() const{
         return tickets;
     }
+
+    /**
+     * Returns the clients BST
+     * @return the clients BST
+     */
     BST<ClientPointer> getClientsBST() const{
         return clients;
     }
+
+    /**
+     * Returns the company BST
+     * @return the company BST
+     */
     BST<CompanyPointer> getCompanyBST() const{
         return company;
     }
 
+    /**
+     * Adds an airport to the airports BST
+     * @param a pointer to Airport that'll be added
+     * @return bool upon success, false otherwise
+     */
     bool addAirport(Airport* a){
         AirportPointer aptr(a);
         return airports.insert(aptr);
     }
+
+    /**
+     * Adds a voyage to the voyages BST
+     * @param v pointer to the Voyage that'll be added
+     * @return bool upon success. false otherwise
+     */
     bool addVoyage(Voyage* v){
         VoyagePointer vptr(v);
         return voyages.insert(vptr);
     }
+
+    /**
+     * Adds a flight to the voyages BST
+     * @param f pointer to the flight that'll be added
+     * @return bool upon success. false otherwise
+     */
     bool addFlight(Flight* f);
+
+    /**
+     * Adds a plane to the planes BST
+     * @param p pointer the plane that'll be added
+     * @return bool upon success. false otherwise
+     */
     bool addPlane(Plane* p){
         PlanePointer pptr(p);
         return planes.insert(pptr);
     }
+
+    /**
+     * Adds a ticket to the tickets BST
+     * @param t pointer to the ticket that'll be added
+     * @return bool upon success. false otherwise
+     */
     bool addTicket(Ticket* t){
         TicketPointer tptr(t);
         return tickets.insert(tptr);
     }
+
+    /**
+     * Adds a client account to the clients BST
+     * @param c poitner to the client account that'll be added
+     * @return bool upon success. false otherwise
+     */
     bool addClient(Client* c){
         ClientPointer cptr(c);
         return clients.insert(cptr);
     }
+
+    /**
+     * Adds a company account to the company BST
+     * @param c pointer to the company account that'll be added
+     * @return bool upon success. false otherwise
+     */
     bool addCompany(Company* c){
         CompanyPointer cptr(c);
         return company.insert(cptr);
